93b54adbf5dcd8813cb1377842bd13f1dbd52ef8
Santii05-C <santi.c.pgm@gmail.com>
2025-02-09 10:41:50 -0300

    last commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,16 @@ function App() {
         <div className="absolute inset-0 bg-gradient-to-br from-gray-900 via-gray-800 to-gray-900 opacity-80" />
         <div className="absolute inset-0 backdrop-blur-sm" />
       </div>
+
+      <Routes>
+        <Route path="/" element={<OverviewPage />} />
+        {/* <Route path='/products' element={<ProductsPage />} />
+				<Route path='/users' element={<UsersPage />} />
+				<Route path='/sales' element={<SalesPage />} />
+				<Route path='/orders' element={<OrdersPage />} />
+				<Route path='/analytics' element={<AnalyticsPage />} />
+				<Route path='/settings' element={<SettingsPage />} /> */}
+      </Routes>
     </div>
   );
 }
