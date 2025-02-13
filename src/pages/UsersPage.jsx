dab4c20a58f8bfbd6a77a5ce852c42d5bb160374
Santii05-C <santi.c.pgm@gmail.com>
2025-02-13 10:25:25 -0300

    last commit

--- src/pages/UsersPage.jsx
+++ src/pages/UsersPage.jsx
@@ -1,7 +1,53 @@
 import { UserCheck, UserPlus, UsersIcon, UserX } from "lucide-react";
 import { motion } from "framer-motion";
+
+import Header from "../components/common/Header";
+import StatCard from "../components/common/StatCard";
+import UsersTable from "../components/users/UsersTable";
+
 const UsersPage = () => {
-  return <div>UsersPage</div>;
+  return (
+    <div className="flex-1 overflow-auto relative z-10">
+      <Header title="Users" />
+
+      <main className="max-w-7xl mx-auto py-6 px-4 lg:px-8">
+        {/* STATS */}
+        <motion.div
+          className="grid grid-cols-1 gap-5 sm:grid-cols-2 lg:grid-cols-4 mb-8"
+          initial={{ opacity: 0, y: 20 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <StatCard
+            name="Total Users"
+            icon={UsersIcon}
+            value={userStats.totalUsers.toLocaleString()}
+            color="#6366F1"
+          />
+          <StatCard
+            name="New Users Today"
+            icon={UserPlus}
+            value={userStats.newUsersToday}
+            color="#10B981"
+          />
+          <StatCard
+            name="Active Users"
+            icon={UserCheck}
+            value={userStats.activeUsers.toLocaleString()}
+            color="#F59E0B"
+          />
+          <StatCard
+            name="Churn Rate"
+            icon={UserX}
+            value={userStats.churnRate}
+            color="#EF4444"
+          />
+        </motion.div>
+
+        <UsersTable />
+      </main>
+    </div>
+  );
 };
 
 export default UsersPage;
